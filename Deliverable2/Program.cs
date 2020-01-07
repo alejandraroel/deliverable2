7ec4e7af81289e27bfbffd672073c5ce630ab06d
Alejandra Roel <alejandraroel@Alejandras-MacBook-Air-2.local>
2020-01-06 23:00:49 -0500

    commit 1

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string message = null;
+            string checksum = null;
+
+
+
+            Console.WriteLine("What is your message?");
+            input = Console.ReadLine().ToUpper();
+
+            foreach (char c in input)
+            {
+                int numberValue = Convert.ToInt32(c);
+                message = $"{numberValue}-";
+                Console.WriteLine(message);
+
+
+            }
+
+                Console.WriteLine($"Your encoded message is {message}");
+
+            //Console.WriteLine($"Your checksum is {checksum}");
+
+        }
+    }
+}
